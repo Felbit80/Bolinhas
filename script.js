8b20de354693032cf90504e6cb8e302882453c89
Felbit <140262666+Felbit80@users.noreply.github.com>
2025-02-06 16:31:58 -0300

    Inserção de mais algumas propriedades como: id, velocidade X e Y, direção X e Y, etc. Além da declaração de 4 métodos: posBola(), remover(), imprimir() e controlar(); todos sem comandos ainda.

--- script.js
+++ script.js
@@ -11,14 +11,35 @@ let bolas = []
 let numBolas = 0
 
 class Bola{
-    constructor(){
+    constructor(cativeiro, arrayBolas){
         this.tamanho = Math.round(Math.random()*10)+10
         this.r = Math.floor(Math.random()*255)
         this.g = Math.floor(Math.random()*255)
         this.b = Math.floor(Math.random()*255)
         this.pX = Math.floor(Math.random() - (larguraCativeiro-this.tamanho))
         this.pY = Math.floor(Math.random() - (alturaCativeiro-this.tamanho))
-        this.velocidade = Math.round(Math.random()*5)+5
+        this.velocidadeX = Math.round(Math.random()*5)+5
+        this.velocidadeY = Math.round(Math.random()*5)+5
+        this.direcaoX = (Math.random()*10) < 5 ? 1 : -1
+        this.direcaoY = (Math.random()*10) < 5 ? 1 : -1
+        this.cativeiro=cativeiro
+        this.arrayBolas=arrayBolas
+        this.id=Date.now()+Math.floor(Math.random()*1000000000000)
+        this.imprimir()
+        this.controle=setInterval(this.controlar(), 10)
+        this.bolaDOM = document.getElementById(this.id)
+    }
+    posBola = () => {
+        //Retorna a posição da bolinha
+    }
+    remover = () => {
+        //Remove a bolinha do DOM
+    }
+    imprimir = () => {
+        //Insere a bolinha no DOM
+    }
+    controlar = () => {
+        //Verifica posição e distâncida da bola da borda
     }
 }
 
