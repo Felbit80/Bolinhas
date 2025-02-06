806c4b74027a7206112e56d491cb59d310b95597
Felbit <140262666+Felbit80@users.noreply.github.com>
2025-02-06 15:13:54 -0300

    Declaração da classe Bola e seus parâmetros aleatórios com a interface Math, atualização das variáveis de tamanho do cativeiro quando a janela for redimensionada e inserção de AELs de clique para os botões.

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ let larguraCativeiro = cativeiro.offsetWidth
 let alturaCativeiro = cativeiro.offsetHeight
 let bolas = []
 let numBolas = 0
+
+class Bola{
+    constructor(){
+        this.tamanho = Math.round(Math.random()*10)+10
+        this.r = Math.floor(Math.random()*255)
+        this.g = Math.floor(Math.random()*255)
+        this.b = Math.floor(Math.random()*255)
+        this.pX = Math.floor(Math.random() - (larguraCativeiro-this.tamanho))
+        this.pY = Math.floor(Math.random() - (alturaCativeiro-this.tamanho))
+        this.velocidade = Math.round(Math.random()*5)+5
+    }
+}
+
+window.addEventListener("resize", (el) => {
+    larguraCativeiro = cativeiro.offsetWidth
+    alturaCativeiro = cativeiro.offsetHeight
+})
+btnAdicionar.addEventListener("click", (el) => {
+    const quantasBolinhas = inputBolinhas.value
+    for(let aux = 0; aux < quantasBolinhas; aux++){
+        //Instanciar novas bolinhas
+    }
+})
+btnRemover.addEventListener("click", (el) => {
+    bolas.map((el) => {
+        //Remover todas as bolinhas
+    })
+})
